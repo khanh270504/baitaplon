5270ac60de129aa1d69192913ee626ad868ffef2
Khanh <anhkhanh.27054@gmail.com>
2023-10-24 17:22:57 +0700

    first commit

---
+++ ThongTinHoTro/script.js
@@ -0,0 +1,42 @@
+const rates = {
+    1: { min: 0, max: 50, price: 1678 },
+    2: { min: 51, max: 100, price: 1734 },
+    3: { min: 101, max: 200, price: 2014 },
+    4: { min: 201, max: 300, price: 2536 }
+};
+
+const devicesData = [];
+
+document.getElementById('calculate').addEventListener('click', function() {
+    const power = parseFloat(document.getElementById('power').value);
+    const hours = parseFloat(document.getElementById('hours').value);
+
+    const energyConsumed = (power * hours) / 1000; // kWh
+    let cost = 0;
+
+    for (let rateKey in rates) {
+        const rate = rates[rateKey];
+        if (energyConsumed >= rate.min && energyConsumed <= rate.max) {
+            cost = energyConsumed * rate.price;
+            break;
+        }
+    }
+
+    devicesData.push({ power, hours, cost });
+
+    displayResults();
+});
+
+function displayResults() {
+    let totalCost = 0;
+    let resultText = '';
+
+    for (let i = 0; i < devicesData.length; i++) {
+        resultText += `Thiết bị ${i + 1} - Tiền điện: ${devicesData[i].cost.toFixed(2)} VNĐ<br>`;
+        totalCost += devicesData[i].cost;
+    }
+
+    resultText += `<br>Tổng tiền điện của tất cả các thiết bị: ${totalCost.toFixed(2)} VNĐ`;
+
+    document.getElementById('result').innerHTML = resultText;
+}
