23a4493642e74fb082485fcc9ec77bc49944c7a8
khanh270504 <124353786+khanh270504@users.noreply.github.com>
2023-10-26 18:37:04 +0700

    Update script.js

--- ThongTinHoTro/script.js
+++ ThongTinHoTro/script.js
@@ -40,3 +40,21 @@ function displayResults() {
 
     document.getElementById('result').innerHTML = resultText;
 }
+
+//nhap vao chuong
+ const chuong = document.querySelector('.fa-bell');
+
+ chuong.onclick = function() {
+   alert('Bạn có thông báo mới!');
+ };
+ 
+ // Thanh cong cu tim kiem
+ const timkiem = document.querySelector('.form-control');
+ const searchIcon = document.querySelector('.fa-search');
+  searchIcon.onclick = function() {
+   const searchText = timkiem.value.trim();
+ 
+   if (searchText === '') {
+     alert('Vui lòng nhập từ khóa tìm kiếm!');
+   } 
+ };
